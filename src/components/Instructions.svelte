6cf3c1b76d2797af4eb8c000cfa7eb53c58c94c5
Nic <42087659+nicm42@users.noreply.github.com>
2021-09-06 19:30:02 +0100

    Added condition to say whether to show first line of instructions

--- src/components/Instructions.svelte
+++ src/components/Instructions.svelte
@@ -2,8 +2,10 @@
   export let shape: string;
 </script>
 
-	<div class="instructions">
-    <p><span class="instructions-select">Select a shape.</span></p>
-    <p>Click the <span class="instructions-shape">{shape}</span></p>
-    <p>Click it again when it's twice the size.</p>
-  </div>
+<div class="instructions">
+  {#if shape === 'shape'}
+    <p>Select a shape.</p>
+  {/if}
+  <p>Click the {shape}.</p>
+  <p>Click it again when it's twice the size.</p>
+</div>
