f7598d73f56735c9f35cdc6815d7890d625b78ae
Nic <42087659+nicm42@users.noreply.github.com>
2021-10-03 20:24:41 +0100

    Updated tests to use screen so it's more readable

--- src/components/Instructions.test.js
+++ src/components/Instructions.test.js
@@ -3,33 +3,34 @@
  */
 
 import '@testing-library/jest-dom';
-import { render } from '@testing-library/svelte'
+import { render, screen } from '@testing-library/svelte'
 import Instructions from './Instructions.svelte';
 
+
 test('Text is correct when no shape has been selected', () => {
-  const {getByText} = render(Instructions, {shape: 'shape'})
-  expect(getByText('Select a shape.')).toBeInTheDocument();
-  expect(getByText('Click the shape.')).toBeInTheDocument();
-  expect(getByText('Click it again when it\'s twice the size.')).toBeInTheDocument();
+  render(Instructions, {shape: 'shape'});
+  expect(screen.getByText('Select a shape.')).toBeInTheDocument();
+  expect(screen.getByText('Click the shape.')).toBeInTheDocument();
+  expect(screen.getByText('Click it again when it\'s twice the size.')).toBeInTheDocument();
 })
 
 test('Text is correct when shape is a circle', () => {
-  const {queryByText} = render(Instructions, {shape: 'Circle'})
-  expect(queryByText('Select a shape.')).not.toBeInTheDocument();
-  expect(queryByText('Click the circle.')).toBeInTheDocument();
-  expect(queryByText('Click it again when it\'s twice the size.')).toBeInTheDocument();
+  render(Instructions, {shape: 'Circle'});
+  expect(screen.queryByText('Select a shape.')).not.toBeInTheDocument();
+  expect(screen.getByText('Click the circle.')).toBeInTheDocument();
+  expect(screen.getByText('Click it again when it\'s twice the size.')).toBeInTheDocument();
 })
 
 test('Text is correct when shape is a square', () => {
-  const {queryByText} = render(Instructions, {shape: 'Square'})
-  expect(queryByText('Select a shape.')).not.toBeInTheDocument();
-  expect(queryByText('Click the square.')).toBeInTheDocument();
-  expect(queryByText('Click it again when it\'s twice the size.')).toBeInTheDocument();
+  render(Instructions, {shape: 'Square'});
+  expect(screen.queryByText('Select a shape.')).not.toBeInTheDocument();
+  expect(screen.getByText('Click the square.')).toBeInTheDocument();
+  expect(screen.getByText('Click it again when it\'s twice the size.')).toBeInTheDocument();
 })
 
 test('Text is correct when shape is a triangle', () => {
-  const {queryByText} = render(Instructions, {shape: 'Triangle'})
-  expect(queryByText('Select a shape.')).not.toBeInTheDocument();
-  expect(queryByText('Click the triangle.')).toBeInTheDocument();
-  expect(queryByText('Click it again when it\'s twice the size.')).toBeInTheDocument();
+  render(Instructions, {shape: 'Triangle'});
+  expect(screen.queryByText('Select a shape.')).not.toBeInTheDocument();
+  expect(screen.getByText('Click the triangle.')).toBeInTheDocument();
+  expect(screen.getByText('Click it again when it\'s twice the size.')).toBeInTheDocument();
 })
