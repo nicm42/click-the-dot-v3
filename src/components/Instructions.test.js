bbdb55d3c1720f071cb739aec0a98f0ff24da6da
Nic <42087659+nicm42@users.noreply.github.com>
2021-10-03 20:15:53 +0100

    Added tests for all the shapes

--- src/components/Instructions.test.js
+++ src/components/Instructions.test.js
@@ -8,5 +8,28 @@ import Instructions from './Instructions.svelte';
 
 test('Text is correct when no shape has been selected', () => {
   const {getByText} = render(Instructions, {shape: 'shape'})
-  expect(getByText('Select a shape.')).toBeInTheDocument()
+  expect(getByText('Select a shape.')).toBeInTheDocument();
+  expect(getByText('Click the shape.')).toBeInTheDocument();
+  expect(getByText('Click it again when it\'s twice the size.')).toBeInTheDocument();
+})
+
+test('Text is correct when shape is a circle', () => {
+  const {queryByText} = render(Instructions, {shape: 'Circle'})
+  expect(queryByText('Select a shape.')).not.toBeInTheDocument();
+  expect(queryByText('Click the circle.')).toBeInTheDocument();
+  expect(queryByText('Click it again when it\'s twice the size.')).toBeInTheDocument();
+})
+
+test('Text is correct when shape is a square', () => {
+  const {queryByText} = render(Instructions, {shape: 'Square'})
+  expect(queryByText('Select a shape.')).not.toBeInTheDocument();
+  expect(queryByText('Click the square.')).toBeInTheDocument();
+  expect(queryByText('Click it again when it\'s twice the size.')).toBeInTheDocument();
+})
+
+test('Text is correct when shape is a triangle', () => {
+  const {queryByText} = render(Instructions, {shape: 'Triangle'})
+  expect(queryByText('Select a shape.')).not.toBeInTheDocument();
+  expect(queryByText('Click the triangle.')).toBeInTheDocument();
+  expect(queryByText('Click it again when it\'s twice the size.')).toBeInTheDocument();
 })
