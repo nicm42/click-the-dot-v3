0cfab6fcf50c3799f2b28f945db9fe018197e8c1
Nic <42087659+nicm42@users.noreply.github.com>
2021-10-04 17:06:35 +0100

    Added tests for Results text and button

---
+++ src/components/Result.test.js
@@ -0,0 +1,15 @@
+/**
+ * @jest-environment jsdom
+ */
+
+import '@testing-library/jest-dom';
+import { render, screen } from '@testing-library/svelte'
+import Result from './Result.svelte';
+
+test('Text is all there', () => {
+  const resultTextStart = 'Missed!';
+  render(Result, {ratio: 1.2, reset: false});
+  expect(screen.getByText('Missed!')).toBeInTheDocument();
+  expect(screen.getByText('The shape is now 1.2 times its original size')).toBeInTheDocument();
+  expect(screen.getByRole('button', {name: /Try again/i})).toBeInTheDocument();
+})
