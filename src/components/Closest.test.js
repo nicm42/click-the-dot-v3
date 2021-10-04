486eec6c5813c00a4f95241d70b24a1761f5bd97
Nic <42087659+nicm42@users.noreply.github.com>
2021-10-04 17:20:15 +0100

    Added tests for Closest shapes

--- src/components/Closest.test.js
+++ src/components/Closest.test.js
@@ -14,3 +14,12 @@ test('Text is all there', () => {
   expect(screen.getByText('1.8')).toBeInTheDocument();
   expect(screen.queryByText('3.0')).not.toBeInTheDocument();
 })
+
+test('Shapes are all there', () => {
+  const shapes = ['Circle', 'Square', 'Triangle'];
+  const scores = {'Circle': 1.2, 'Square': 1.8, 'Triangle': 3.0};
+  render(Closest, {shapes: shapes, scores: scores});
+  expect(screen.getByTestId('circle')).toBeInTheDocument();
+  expect(screen.getByTestId('square')).toBeInTheDocument();
+  expect(screen.getByTestId('triangle')).toBeInTheDocument();
+})
