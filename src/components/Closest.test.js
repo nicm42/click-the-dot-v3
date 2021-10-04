d95a908d3d5355e0ab87ddfb238a11287357d1e4
Nic <42087659+nicm42@users.noreply.github.com>
2021-10-04 17:14:44 +0100

    Added test for Closest text

---
+++ src/components/Closest.test.js
@@ -0,0 +1,16 @@
+/**
+ * @jest-environment jsdom
+ */
+
+import '@testing-library/jest-dom';
+import { render, screen } from '@testing-library/svelte'
+import Closest from './Closest.svelte';
+
+test('Text is all there', () => {
+  const shapes = ['Circle', 'Square', 'Triangle'];
+  const scores = {'Circle': 1.2, 'Square': 1.8, 'Triangle': 3.0};
+  render(Closest, {shapes: shapes, scores: scores});
+  expect(screen.getByText('1.2')).toBeInTheDocument();
+  expect(screen.getByText('1.8')).toBeInTheDocument();
+  expect(screen.queryByText('3.0')).not.toBeInTheDocument();
+})
