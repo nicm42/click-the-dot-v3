a4a11859d98b6d904c9c377ff6ce1c68a7862d97
Nic <42087659+nicm42@users.noreply.github.com>
2021-10-05 17:12:29 +0100

    Added test to check shape stops on second click

--- src/components/Shape.test.js
+++ src/components/Shape.test.js
@@ -6,7 +6,7 @@ import '@testing-library/jest-dom';
 import { render, screen, fireEvent } from '@testing-library/svelte'
 import Shape from './Shape.svelte';
 
-jest.setTimeout(8000); // Tween takes max 7 seconds
+jest.setTimeout(20000); // Tween takes max 7 seconds
 
 test('Circles are there', () => {
   const scores = {'Circle': 1.2, 'Square': 1.8, 'Triangle': 3.0};
@@ -46,14 +46,31 @@ test('State after finished growing', () => {
   expect(screen.getByTestId('helper-svg')).toBeVisible();
 })
 
-test.only('Shape grows on click', async () => {
+test('Shape grows on click', async () => {
   const scores = {'Circle': 1.2, 'Square': 1.8, 'Triangle': 3.0};
   render(Shape, {shape: 'Circle', finishedGrowing: false, ratio: 2.0, scores: scores, reset: false, initialSize: '100px'});
+  expect(screen.getByTestId('shape-svg')).not.toHaveClass('clicked');
   expect(screen.getByTestId('shape-svg')).toHaveStyle('transform: scale(1)');
   fireEvent.click(screen.getAllByTestId('circle')[0]);
   // Wait for max tween time before checking it's done anything
   await new Promise((r) => setTimeout(r, 7000));
   expect(screen.getByTestId('shape-svg')).toHaveClass('clicked');
   expect(screen.getByTestId('shape-svg')).toHaveStyle('transform: scale(3)');
+  expect(localStorage.getItem('Circle')).toBe('3.0');
+})
+
+test('Shape stops growing on second click', async () => {
+  const scores = {'Circle': 1.2, 'Square': 1.8, 'Triangle': 3.0};
+  render(Shape, {shape: 'Circle', finishedGrowing: false, ratio: 2.0, scores: scores, reset: false, initialSize: '100px'});
+  expect(screen.getByTestId('shape-svg')).not.toHaveClass('clicked');
+  expect(screen.getByTestId('shape-svg')).toHaveStyle('transform: scale(1)');
+  fireEvent.click(screen.getAllByTestId('circle')[0]);
+  // Wait for a bit before clicking again
+  await new Promise((r) => setTimeout(r, 1000));
+  fireEvent.click(screen.getAllByTestId('circle')[0]);
+  await new Promise((r) => setTimeout(r, 1000));
+  expect(screen.getByTestId('shape-svg')).toHaveClass('clicked');
+  expect(localStorage.getItem('Circle')).toBeTruthy();
+  expect(localStorage.getItem('Circle')).not.toBe('3.0');
 })
 
