03a9c3f2b415085e834ee556c9ab53b1b4965b4c
Nic <42087659+nicm42@users.noreply.github.com>
2021-10-10 14:53:17 +0100

    Added tests for shape change on select update

---
+++ cypress/integration/select.test.ts
@@ -0,0 +1,33 @@
+/// <reference types="Cypress" />
+/// <reference types="@testing-library/cypress" />
+
+describe('Post tests', () => {
+  beforeEach(() => {
+    cy.visit(Cypress.config('baseUrl'));
+  });
+
+  it('Shape changes when select changes', () => {
+    cy.findByTestId('select')
+      .should('exist')
+      .should('have.value', null);
+    
+    // Circle
+    cy.findByTestId('select')
+    .select('Circle')
+    .should('have.value', 'Circle');
+    // There's already a shape in closest, so now there should be shape and helper as well
+    cy.findAllByTestId('circle').should('have.length', 3);
+
+    // Square
+    cy.findByTestId('select')
+    .select('Square')
+    .should('have.value', 'Square');
+    cy.findAllByTestId('square').should('have.length', 3);
+
+    // Triangle
+    cy.findByTestId('select')
+    .select('Triangle')
+    .should('have.value', 'Triangle');
+    cy.findAllByTestId('triangle').should('have.length', 3);
+  });
+});
