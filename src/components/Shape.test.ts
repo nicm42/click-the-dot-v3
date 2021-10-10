49168f97350948bbaf66ed1eb125685122f18acb
Nic <42087659+nicm42@users.noreply.github.com>
2021-10-10 19:21:03 +0100

    Added Jest test for pressing spacebar on shape

--- src/components/Shape.test.ts
+++ src/components/Shape.test.ts
@@ -68,6 +68,34 @@ test('Shape stops growing on second click', async () => {
   expect(localStorage.getItem('Circle')).not.toBe('3.0');
 })
 
+test('Shape grows on space press', async () => {
+  const scores = {'Circle': 1.2, 'Square': 1.8, 'Triangle': 3.0};
+  render(Shape, {shape: 'Circle', isFinishedGrowing: false, ratio: 2.0, scores: scores, isReset: false, initialSize: '100px'});
+  expect(screen.getByTestId('shape-svg')).not.toHaveClass('clicked');
+  expect(screen.getByTestId('shape-svg')).toHaveStyle('transform: scale(1)');
+  fireEvent.keyPress(screen.getAllByTestId('circle')[0], {key: ' '});
+  // Wait for max tween time before checking it's done anything
+  await new Promise((r) => setTimeout(r, 7000));
+  expect(screen.getByTestId('shape-svg')).toHaveClass('clicked');
+  expect(screen.getByTestId('shape-svg')).toHaveStyle('transform: scale(3)');
+  expect(localStorage.getItem('Circle')).toBe('3.0');
+})
+
+test('Shape stops growing on second space press', async () => {
+  const scores = {'Circle': 1.2, 'Square': 1.8, 'Triangle': 3.0};
+  render(Shape, {shape: 'Circle', isFinishedGrowing: false, ratio: 2.0, scores: scores, isReset: false, initialSize: '100px'});
+  expect(screen.getByTestId('shape-svg')).not.toHaveClass('clicked');
+  expect(screen.getByTestId('shape-svg')).toHaveStyle('transform: scale(1)');
+  fireEvent.keyPress(screen.getAllByTestId('circle')[0], {key: ' ', code: 'Space'});
+  // Wait for a bit before clicking again
+  await new Promise((r) => setTimeout(r, 1000));
+  fireEvent.keyPress(screen.getAllByTestId('circle')[0], {key: ' ', code: 'Space'});
+  await new Promise((r) => setTimeout(r, 1000));
+  expect(screen.getByTestId('shape-svg')).toHaveClass('clicked');
+  expect(localStorage.getItem('Circle')).toBeTruthy();
+  expect(localStorage.getItem('Circle')).not.toBe('3.0');
+})
+
 test('localStorage is only updated if new score is closer to 2.0', async () => {
   const scores = {'Circle': 1.2, 'Square': 1.8, 'Triangle': 3.0};
   render(Shape, {shape: 'Circle', isFinishedGrowing: false, ratio: 2.0, scores: scores, isReset: false, initialSize: '100px'});
