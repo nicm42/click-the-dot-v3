e8bfc7d998ccbb16347e123547a0cfd5a892e78a
Nic <42087659+nicm42@users.noreply.github.com>
2021-10-13 19:45:15 +0100

    Added Express server

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,11 +1,19 @@
 import express, { Request, Response } from 'express';
 //const path = require('path');
+const mongoose = require('mongoose');
 require('dotenv').config();
 
 const app = express();
 const port = process.env.PORT || 8000;
 
-//console.log(__dirname);
+// Connect to MongoDB
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+}).then((result: any) => app.listen(port, () => {
+  console.log(`Listening at http://localhost:${port}`);
+}))
+  .catch((err: any) => console.log(err))
 
 // Any other request goes to index
 /* app.get('*', function (req, res) {
@@ -16,6 +24,6 @@ app.get('/', async (req: Request, res: Response) => {
   res.send('Sending res');
 })
 
-module.exports = app.listen(port, () => {
+/* module.exports = app.listen(port, () => {
   console.log(`Listening at http://localhost:${port}`);
-});
+}); */
