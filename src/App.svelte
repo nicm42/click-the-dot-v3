f421947033bb747181a4134e9b695f5a6c4880ac
Nic <42087659+nicm42@users.noreply.github.com>
2021-10-17 21:11:31 +0100

    Fixed TS error on Google Font link

--- src/App.svelte
+++ src/App.svelte
@@ -54,7 +54,7 @@
 
 <svelte:head>
   <link rel="preconnect" href="https://fonts.googleapis.com" />
-  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="anonymous" />
   <link
     href="https://fonts.googleapis.com/css2?family=Ubuntu:wght@400;700&display=swap"
     rel="stylesheet"
