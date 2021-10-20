7ccf9109867b8ceec00726bb30fc947c6491227b
Nic <42087659+nicm42@users.noreply.github.com>
2021-10-20 19:34:19 +0100

    Added static option to serve front end

--- src/server/index.js
+++ src/server/index.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 var cors = require("cors");
+const path = require('path');
 const mongoose = require('mongoose');
 
 const app = express();
@@ -10,13 +11,16 @@ const port = process.env.PORT || 8000;
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true
-}).then((result) => app.listen(port, () => {
+}).then(() => app.listen(port, () => {
   console.log(`Listening at http://localhost:${port}`);
 }))
   .catch((err) => console.log(err))
 
 app.use(cors());
 app.use(express.json());
+
+// Serve up the front end
+app.use(express.static(path.resolve(__dirname, '..', '..', 'public')));
 
 // Routes
 app.use('/api/getscores', require('./routes/getScores'))
